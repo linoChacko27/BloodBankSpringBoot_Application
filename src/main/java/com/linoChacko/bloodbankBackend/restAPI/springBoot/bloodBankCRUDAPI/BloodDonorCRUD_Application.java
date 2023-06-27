9ccb2f7c60aabfccc6cce4147fc83a2d0a61f30d
linoChacko27 <linochacko2000@gmail.com>
2023-06-27 20:45:26 +0530

    Pipeline testing

--- src/main/java/com/linoChacko/bloodbankBackend/restAPI/springBoot/bloodBankCRUDAPI/BloodDonorCRUD_Application.java
+++ src/main/java/com/linoChacko/bloodbankBackend/restAPI/springBoot/bloodBankCRUDAPI/BloodDonorCRUD_Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BloodDonorCRUD_Application {
- //testing
+ //testing123
 	public static void main(String[] args) {
 		SpringApplication.run(BloodDonorCRUD_Application.class, args);
 	}
