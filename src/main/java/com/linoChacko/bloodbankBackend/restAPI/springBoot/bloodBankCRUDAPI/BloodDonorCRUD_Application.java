c2eb7bf88ac8ce74ee0f416a6d0aac81a9b6f4e3
linoChacko27 <linochacko2000@gmail.com>
2023-06-27 20:50:50 +0530

    Pipeline testing1

--- src/main/java/com/linoChacko/bloodbankBackend/restAPI/springBoot/bloodBankCRUDAPI/BloodDonorCRUD_Application.java
+++ src/main/java/com/linoChacko/bloodbankBackend/restAPI/springBoot/bloodBankCRUDAPI/BloodDonorCRUD_Application.java
@@ -9,5 +9,6 @@ public class BloodDonorCRUD_Application {
 	public static void main(String[] args) {
 		SpringApplication.run(BloodDonorCRUD_Application.class, args);
 	}
- //
+ //yuoiusbshsgysgfsghsgy
+
 }
